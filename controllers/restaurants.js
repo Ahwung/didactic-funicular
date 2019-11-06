7734be88642098b64a913909071e5dfffeb0dfe9
Adam Hwung <Ahwung@gmail.com>
2019-11-06 14:50:18 -0800

    created New page and added route to create data

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,8 +1,17 @@
 const express = require("express")
+const Restaurant = require('../models/restaurants.js')
 const router = express.Router()
 
+// Route to get to New page
 router.get("/new", (req, res) => {
 	res.render('restaurants/new.ejs')
+})
+
+// Route to create entries in the collection
+router.post('/', (req, res) => {
+	Restaurant.create(req.body, (err, createdRestaurant) => {
+		res.redirect("/restaurants")
+	})
 })
 
 module.exports = router
---
+++ models/restaurants.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+// Creating the schema for any new restaurant
+const restaurantSchema = new mongoose.Schema({
+	name: { type: String, required: true},
+	img: { type: String, required: true},
+	cuisine: {type: String, required: true},
+	city: {type: String, required: true},
+	address: {type: String, required: true},
+	dishes: [{type: String}]
+})
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+
+module.exports = Restaurant
