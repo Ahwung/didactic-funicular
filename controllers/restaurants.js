12ab9173eb5c075106abb0fbc05ee0fc2e0906bf
Adam Hwung <Ahwung@gmail.com>
2019-11-06 15:06:04 -0800

    Created index page and index route

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -7,6 +7,15 @@ router.get("/new", (req, res) => {
 	res.render('restaurants/new.ejs')
 })
 
+// Route to get to Index Page
+router.get("/", (req, res) => {
+	Restaurant.find({}, (error, allRestaurants) => {
+		res.render('restaurants/index.ejs', {
+			restaurants: allRestaurants
+		})
+	})
+})
+
 // Route to create entries in the collection
 router.post('/', (req, res) => {
 	Restaurant.create(req.body, (err, createdRestaurant) => {
