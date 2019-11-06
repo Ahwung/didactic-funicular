c1cf4bda243cc71ba1962b9a4e9fb66a5318714d
Adam Hwung <Ahwung@gmail.com>
2019-11-06 15:13:53 -0800

    added seed data

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -2,6 +2,40 @@ const express = require("express")
 const Restaurant = require('../models/restaurants.js')
 const router = express.Router()
 
+router.get("/seed", (req, res) => {
+	Restaurant.create(
+		[
+			{
+				name: "Meet Fresh",
+				img: "https://s3-media2.fl.yelpcdn.com/bphoto/NDzaVnG3KtWZK1s_1Z1olA/l.jpg",
+				cuisine: "Dessert",
+				city: "Daly City",
+				address: "148C Serramonte Ctr, Daly City, CA 94015",
+				dishes: ["Q Mochi", "Hot Grass Jelly"]
+			},
+			{
+				name: "Cheese Board Pizza",
+				img: "https://s3-media3.fl.yelpcdn.com/bphoto/ZKFWsPKioIppHqJWPo2bVw/o.jpg",
+				cuisine: "Pizza",
+				city: "Berkeley",
+				address: "1512 Shattuck Ave, Berkeley, CA 94709",
+				dishes: ["Vegetarian Pizza", "Corn Pizza"]
+			},
+			{
+				name: "Demiya",
+				img: "https://s3-media4.fl.yelpcdn.com/bphoto/NOyuTiqQCDeZzDtbFL-_7g/o.jpg",
+				cuisine: "Japanese",
+				city: "San Jose",
+				address: "375 Saratoga Ave Ste G, San Jose, CA 95129",
+				dishes: ["Japanese Curry", "Chicken Katsu Curry"]
+			}
+		],
+		(err, data) => {
+			res.redirect("/restaurants");
+		}
+	);
+});
+
 // Route to get to New page
 router.get("/new", (req, res) => {
 	res.render('restaurants/new.ejs')
