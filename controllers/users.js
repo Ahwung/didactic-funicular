5d2501c453585e54d3980c30d08c1cc3a7e4e2b1
Adam Hwung <Ahwung@gmail.com>
2019-11-06 21:57:06 -0800

    Fixing errors

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,6 @@ router.get('/new', (req, res) => {
 router.post('/', (req, res) => {
 	req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
 	User.create(req.body, (error, createdUser) => {
-		req.session.username = createdUser.username
 		res.redirect('/restaurants')
 	})
 })
