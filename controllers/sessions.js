e289d606c9961a5e451036e98ff7053a7e045ce9
Adam Hwung <Ahwung@gmail.com>
2019-11-07 14:56:35 -0800

    console logging username

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -15,6 +15,7 @@ router.post('/', (req, res) => {
 			const doesPasswordMatch = bcrypt.compareSync(req.body.password, foundUser.password)
 			if (doesPasswordMatch) {
 				req.session.username = foundUser.username
+				console.log(req.session.username)
 				res.redirect('/restaurants')
 			} else {
 				res.redirect('/sessions/new')
